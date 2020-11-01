ba181624e41492eafd0a9cf52c9573af3dc45e21
David Skyberg <davidskyberg@gmail.com>
2020-11-01 09:12:05 -0500

    Deleted unused interface.  Added error logging.

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import ProjectList from '../components/ProjectList';
-import { GET_PROJECTS } from '../schema'
-
-export type CategoryData = {
-   __typeName: string;
-   _id: string;
-   title: string;
-   order: number;
-   titleBackgroundColor: string;
-   backgroundColor: string;
-};
-
+import { GET_PROJECTS } from '../schema';
 
 const ProjectsContainer: React.FC = () => {
    const { loading, error, data } = useQuery(GET_PROJECTS);
    if (loading) return <p>Loading...</p>;
-   if (error) return <p>Error!</p>;
+   if (error) {
+      console.log(error);
+      return <p>Error!</p>;
+   }
    return <ProjectList projects={data.Project} />;
 };
 export default ProjectsContainer;
